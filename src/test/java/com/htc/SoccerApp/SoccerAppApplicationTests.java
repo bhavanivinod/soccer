fce077fd7e29a445609c6298b703d7657d587b27
bhavanivinod <kbhavani86@gmail.com>
2022-06-27 13:18:48 +0530

    Update SoccerAppApplicationTests.java

--- src/test/java/com/htc/SoccerApp/SoccerAppApplicationTests.java
+++ src/test/java/com/htc/SoccerApp/SoccerAppApplicationTests.java
@@ -27,53 +27,53 @@ class SoccerAppApplicationTests {
 
 	}
 	
-	@Test
-	void contextLoads3() {
-		Assertions.assertEquals(new Team(21, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1),teamService.addTeam(new Team(21, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1)));
+// 	@Test
+// 	void contextLoads3() {
+// 		Assertions.assertEquals(new Team(21, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1),teamService.addTeam(new Team(21, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1)));
 
-	}
+// 	}
 	
-	@Test
-	void contextLoads4() {
-		Assertions.assertEquals(new Team(20, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1),teamService.addTeam(new Team(20, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1)));
+// 	@Test
+// 	void contextLoads4() {
+// 		Assertions.assertEquals(new Team(20, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1),teamService.addTeam(new Team(20, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1)));
 
-	}
+// 	}
 	
-	@Test
-	void contextLoads5() {
-		Assertions.assertEquals(new Team(17, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1),teamService.addTeam(new Team(17, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1)));
+// 	@Test
+// 	void contextLoads5() {
+// 		Assertions.assertEquals(new Team(17, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1),teamService.addTeam(new Team(17, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1)));
 
-	}
+// 	}
 	
-	@Test
-	void contextLoads6() {
-		Assertions.assertEquals(new Team(16, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1),teamService.addTeam(new Team(16, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1)));
+// 	@Test
+// 	void contextLoads6() {
+// 		Assertions.assertEquals(new Team(16, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1),teamService.addTeam(new Team(16, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1)));
 
-	}
+// 	}
 	
-	@Test
-	void contextLoads7() {
-		Assertions.assertEquals(new Team(12, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1),teamService.addTeam(new Team(12, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1)));
+// 	@Test
+// 	void contextLoads7() {
+// 		Assertions.assertEquals(new Team(12, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1),teamService.addTeam(new Team(12, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1)));
 
-	}
+// 	}
 	
-	@Test
-	void contextLoads8() {
-		Assertions.assertEquals(new Team(15, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1),teamService.addTeam(new Team(15, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1)));
+// 	@Test
+// 	void contextLoads8() {
+// 		Assertions.assertEquals(new Team(15, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1),teamService.addTeam(new Team(15, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1)));
 
-	}
+// 	}
 	
-	@Test
-	void contextLoads9() {
-		Assertions.assertEquals(new Team(13, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1),teamService.addTeam(new Team(13, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1)));
+// 	@Test
+// 	void contextLoads9() {
+// 		Assertions.assertEquals(new Team(13, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1),teamService.addTeam(new Team(13, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1)));
 
-	}
+// 	}
 	
-	@Test
-	void contextLoads10() {
-		Assertions.assertEquals(new Team(14, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1),teamService.addTeam(new Team(14, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1)));
+// 	@Test
+// 	void contextLoads10() {
+// 		Assertions.assertEquals(new Team(14, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1),teamService.addTeam(new Team(14, "ABC", new Date(2002, 02, 02), new Date(2002, 02, 02),1)));
 
-	}
+// 	}
 	
 
 
